66be1c61a02ff21e3fbb71f29b8a867c79eb5ad4
Rodro-Kumar <rudrokormokar96@gmail.com>
2024-09-19 22:03:56 +0600

    modified

--- cashout.js
+++ cashout.js
@@ -1,11 +1,8 @@
 document.getElementById("cash-out").addEventListener("click", function (event) {
   event.preventDefault();
   const amount = document.getElementById("cashout-amount").value;
-  console.log(amount);
-
   const pin = document.getElementById("cashout-pin").value;
   const balance = document.getElementById("main-balance").innerText;
-  console.log(balance);
 
   if (pin === "1234" && amount <= balance) {
     const ammountNumber = parseFloat(amount);
